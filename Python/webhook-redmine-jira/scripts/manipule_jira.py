3edf60d049d31b58c519682abf412d13e22d4332
eduardo.morais <eduardo.morais@basis.com.br>
2019-04-25 18:59:05 -0300

    criando tarefas do redmine para o jira

--- Python/webhook-redmine-jira/scripts/manipule_jira.py
+++ Python/webhook-redmine-jira/scripts/manipule_jira.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from jira import JIRA
 from manipule_json import get_fields_values
 
@@ -5,18 +7,19 @@ jira = JIRA(server='https://sgotst.basis.com.br', basic_auth=('jelly.service', '
 
 def cerate_dic_fields(webhook_json):
     summary, description, priority, fields_value = get_fields_values(webhook_json)
+
+    print('___________'+ summary, description, priority, str(fields_value) + '___________')
+    print(summary)
+
     dic_field = {
-        "fields": {
-            "project":
-            {
-                "key": "TSTCLIENTE"
-            },
-            "summary": summary,
-            "description": description,
-            "issuetype": {
-                "id": "3"
-            }
-        }
+        'project': {'key': 'TSTBASIS'},
+        'summary': summary,
+        'description': description,
+        'issuetype': {'id': '3'},
+        'customfield_25253': 'N/A',                 #Subtipo
+        'customfield_25254': 'Revisar',             #Ação
+        'customfield_25348': 'Arquitetura',         #Departamento
+        'customfield_25252': 'Código'               #Objeto da Tarefa
     }
 
     return dic_field
